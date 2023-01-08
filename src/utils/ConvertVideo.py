691999c218c256137bf6faffa81884b35fcb0d62
wbcastilho <welber.castilho@yahoo.com.br>
2023-01-08 04:59:59 -0300

    Adicionado utils/ConvertVideo

---
+++ src/utils/ConvertVideo.py
@@ -0,0 +1,18 @@
+import subprocess
+import os
+
+
+class ConvertVideo:
+    @staticmethod
+    def get_duration(video_file: str) -> str:
+        convert_file = 'ffmpeg.exe'
+
+        if not os.path.isfile(convert_file):
+            raise FileNotFoundError(f"Falha ao encontrar executável {convert_file}")
+
+        command = ['ffmpeg', '-i', video_file]
+        ffmpeg = subprocess.Popen(command, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        result = ffmpeg.communicate()
+        str_result = result[1].decode("utf-8")
+        position = str_result.find("Duration")
+        return str_result[position+10:position+21]
