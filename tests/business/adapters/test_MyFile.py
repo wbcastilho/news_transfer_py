c7d48297525d99753bb0488a76ec1b6e32f134f2
wbcastilho <welber.castilho@yahoo.com.br>
2023-01-15 00:32:12 -0300

    Criado test_MyFile

---
+++ tests/business/adapters/test_MyFile.py
@@ -0,0 +1,24 @@
+import os
+import unittest
+from src.business.adapters.MyFile import MyFile
+
+
+class TestMyFile(unittest.TestCase):
+    """ Método chamado antes de cada teste """
+    def setUp(self) -> None:
+        arquivo = open("tests\\files\\file.xml", "a")
+
+    """ Método chamado depois de cada teste """
+    def tearDown(self) -> None:
+        pass
+
+    def test_excluir_arquivo_xml(self):
+        file = MyFile()
+        file.excluir_arquivo_xml('tests\\files\\', 'file')
+
+        if not os.path.exists('tests\\files\\file.xml'):
+            self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
