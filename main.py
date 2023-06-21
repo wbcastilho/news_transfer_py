35efc8aaee6e86d252c62059c9488250f026b9af
welbercastilho <welber.pereira@eptv.com.br>
2023-06-21 13:15:45 -0300

    Alterado o titulo da aplicacao

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import ttkbootstrap as ttk
 
 if __name__ == '__main__':
     app = ttk.Window(
-        title="News Transfer - 2.0",
+        title="News Transfer - 2.1",
         resizable=(False, False)
     )
 
